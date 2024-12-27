60f81b91fa5e40d06c8044a4743fdc1eb56a1229
kjone8371 <rlawjddnsj8371@gmail.com>
2024-12-27 23:53:11 +0900

    DFSExample

--- src/Study1/DFS/DFSExample.java
+++ src/Study1/DFS/DFSExample.java
@@ -21,7 +21,7 @@ public class DFSExample {
         graph.get(0).add(1);
         graph.get(0).add(2);
         graph.get(1).add(3);
-        graph.get(1).add(4);
+        graph.get(1).add (4);
 
         boolean[] visited = new boolean[5];
         dfs(0, visited, graph);
